a78ed3207008d45174b2e049ab31f1d747a288f1
Stephen Allen <allens1015@gmail.com>
2021-07-10 14:16:40 -0400

    added axios/spell view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
 
 new Vue({
   vuetify,
